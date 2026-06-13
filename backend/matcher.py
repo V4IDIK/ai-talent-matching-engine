cca3469e0e840b2a332f2db22d9cfabfef1c0c6e
V4IDIK <vaidik2001@gmail.com>
2026-06-13 10:46:21 +0530

    Added Semantic Similarity matching

--- backend/matcher.py
+++ backend/matcher.py
@@ -1,33 +1,36 @@
 import json
 
+from sklearn.metrics.pairwise import cosine_similarity
+
+from embeddings import get_embedding
+
 with open("data/resume.json") as f:
     resume = json.load(f)
 
 with open("data/jd.json") as f:
     jd = json.load(f)
 
-resume_skills = set(
-    skill.lower()
-    for skill in resume["skills"]
+resume_text = " ".join(
+    resume["skills"]
 )
 
-required_skills = set(
-    skill.lower()
-    for skill in jd["required_skills"]
+jd_text = " ".join(
+    jd["required_skills"]
 )
 
-matches = resume_skills.intersection(
-    required_skills
+resume_embedding = get_embedding(
+    resume_text
 )
 
-score = (
-    len(matches)
-    / len(required_skills)
-) * 100
+jd_embedding = get_embedding(
+    jd_text
+)
 
-print("Match Score:", round(score, 2))
-print()
+score = cosine_similarity(
+    [resume_embedding],
+    [jd_embedding]
+)[0][0]
 
-print("Matched Skills:")
-for skill in matches:
-    print("-", skill)
+print(
+    f"Semantic Match Score: {score*100:.2f}%"
+)
